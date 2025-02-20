83fe0f87845d13f00e1951f92562cb3a5cb26746
EmilianoCBE <e.carrizales1305@gmail.com>
2025-02-19 21:20:10 -0600

    Table created with data

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,49 @@
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridRenderCellParams, renderActionsCell } from '@mui/x-data-grid';
+import { People } from '@/data/people';
 
 export const Home = () => {
+
+  const pageSize = 5
+  const columns = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      minWidth: 150,
+      renderActionsCell: (params: GridRenderCellParams) => <>{params.field}</>
+    },
+    {
+      field: 'category',
+      headerName: 'Category',
+      flex: 1,
+      minWidth: 150,
+      renderActionsCell: (params: GridRenderCellParams) => <>{params.field}</>
+    },
+    {
+      field: 'company',
+      headerName: 'Company',
+      flex: 1,
+      minWidth: 150,
+      renderActionsCell: (params: GridRenderCellParams) => <>{params.field}</>
+    },
+  ]
+
   return (
-    <div>Home</div>
+    <>
+      <DataGrid
+        disableColumnSelector
+        disableRowSelectionOnClick
+        initialState={{
+          pagination: {
+            paginationModel: { pageSize: pageSize, page: 0 },
+          },
+        }}
+        pageSizeOptions={[5]}
+        rows={People}
+        columns={columns}
+        getRowId={(row) => row.id} // Es el key como en el .map, es el id asociado a cada fila
+      />
+
+    </>
   )
 }
